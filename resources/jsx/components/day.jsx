b2eeddaa86b0d85c315938e8eb516bbe910663fb
breeze4 <ambiturner0@gmail.com>
2015-08-26 20:43:41 -0700

    worked on layout and stubbed out some texts

--- resources/jsx/components/day.jsx
+++ resources/jsx/components/day.jsx
@@ -12,9 +12,46 @@ module.exports = React.createClass({
                 {this.state.name}
             </div>
             <div className="day-body">
-                <p>Eating</p>
+                <p>Eating:
+                    <b>2450 Calories</b>
+                    <ul>
+                        <li>200g Protein</li>
+                        <li>300g Carbs</li>
+                        <li>50g Fat</li>
+                    </ul>
+                </p>
+
+                <div className="btn-group day-buttons">
+                    <button className="btn btn-success" type="submit">
+                        Yep!
+                        <span className="fa fa-smile-o"></span>
+                    </button>
+                    <button className="btn btn-danger" type="submit">
+                        Nope
+                        <span className="fa fa-frown-o"></span>
+                    </button>
+                    <button className="btn btn-warning" type="submit">
+                        Close
+                        <span className="fa fa-meh-o"></span>
+                    </button>
+                </div>
 
                 <p>Fitness</p>
+
+                <div className="btn-group day-buttons">
+                    <button className="btn btn-success" type="submit">
+                        Yep!
+                        <span className="fa fa-smile-o"></span>
+                    </button>
+                    <button className="btn btn-danger" type="submit">
+                        Nope
+                        <span className="fa fa-frown-o"></span>
+                    </button>
+                    <button className="btn btn-warning" type="submit">
+                        Close
+                        <span className="fa fa-meh-o"></span>
+                    </button>
+                </div>
             </div>
         </div>
     }
