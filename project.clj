c0a3cd244bb46279603c0cf9a661750e587f878c
breeze4 <ambiturner0@gmail.com>
2015-08-28 19:13:32 -0700

    added echoing websockets, better logging, http-kit for websocket support

--- project.clj
+++ project.clj
@@ -14,7 +14,10 @@
                  [cheshire "5.5.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.firebase/firebase-token-generator "2.0.0"]]
+                 [http-kit "2.1.19"]
+                 [org.clojure/tools.logging "0.3.1"]]
+
   :plugins [[lein-ring "0.9.6"]
             [lein-environ "1.0.0"]]
-  :ring {:handler weekly.core/app})
+  :ring {:handler weekly.core/app}
+  :main weekly.core)
--- src/weekly/core.clj
+++ src/weekly/core.clj
@@ -6,28 +6,50 @@
     [ring.middleware.params :refer [wrap-params]]
     [ring.middleware.json :refer [wrap-json-response]]
     [ring.util.response :refer [response file-response redirect]]
+    [ring.middleware.reload :as reload]
     [weekly.repository :refer [stub-data]]
+    [org.httpkit.server :refer [run-server with-channel on-close on-receive send!]]
+    [clojure.tools.logging :refer [info]]
     ))
 
 (def user-state (atom {}))
 
+(defn- wrap-request-logging [handler]
+  (fn [{:keys [request-method uri] :as req}]
+    (let [resp (handler req)]
+      (info (name request-method) (:status resp)
+            (if-let [qs (:query-string req)]
+              (str uri "?" qs) uri))
+      resp)))
+
+(defn in-dev? [] true)
+
+(defn handler [request]
+  (with-channel request channel
+                (info channel "connected")
+                (on-close channel (fn [status] (println "channel closed: " status)))
+                (on-receive channel (fn [data]              ;; echo it back
+                                      (send! channel data)))))
+
 (defroutes app-routes
            (GET "/" [] (file-response "index.html" {:root "resources"}))
+           (GET "/ws" [] handler)
            (context "/api/:week-id" [week-id]
              (GET "/" [week-id] (response (stub-data week-id))))
            (route/not-found "<h1>Page not found</h1>"))
 
-(defn logging-middleware [handler]
-  (fn [request]
-    (do
-      (prn request)
-      (handler request)
-      )))
-
 (def app
   (-> app-routes
-      (logging-middleware)
+      (wrap-request-logging)
       (wrap-params)
       (wrap-json-response)
       (wrap-file "resources")
       ))
+
+(defn -main [& args]                                        ;; entry point, lein run will pick up and start from here
+  (let [handler (if (in-dev?)                               ;;(in-dev? args)
+                  (reload/wrap-reload app)                  ;; only reload when dev
+                  app)]
+    (run-server handler {:port 3000})))
+
+;;
