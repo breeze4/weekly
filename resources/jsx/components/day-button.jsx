3c2e00c87cf0b328788d314a163dff9e68a6a8b3
breeze4 <ambiturner0@gmail.com>
2015-08-30 21:33:10 -0700

    added Link to day button

--- resources/jsx/components/day-button.jsx
+++ resources/jsx/components/day-button.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 module.exports = React.createClass({
     render: function () {
         return <div className="week-nav-day">
-            {this.props.name}
+            <Link  activeClassName="selected" to="edit" params={{week:"8-5-2015", day: this.props.name}}>
+                {this.props.name}
+            </Link>
+
         </div>
     }
 });
