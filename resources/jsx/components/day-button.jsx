65360d19e7639093eb9c1cfd5129adca3d2867cc
breeze4 <ambiturner0@gmail.com>
2015-09-02 20:29:25 -0700

    cleaned up week nav styles and make it look a little more cohesive

--- resources/jsx/components/day-button.jsx
+++ resources/jsx/components/day-button.jsx
@@ -3,10 +3,31 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 module.exports = React.createClass({
+    mixins: [Router.State],
+    getInitialState: function () {
+        var params = this.getParams();
+        return {
+            selected: params && params.day === this.props.name
+        }
+    },
+    componentWillReceiveProps: function (newProps) {
+        var params = this.getParams();
+        if (params && params.day === this.props.name) {
+            this.setState({
+                selected: true
+            })
+        } else {
+            this.setState({
+                selected: false
+            })
+        }
+    },
     render: function () {
         return <div className="week-nav-day">
             <Link activeClassName="selected" to="edit" params={{week:"8-5-2015", day: this.props.name}}>
-                {this.props.name}
+                <div className={"week-nav-link-container" + (this.state.selected ? " selected" : "")}>
+                    {this.props.name}
+                </div>
             </Link>
 
             <div className="preview-status-button-container">
@@ -17,15 +38,19 @@ module.exports = React.createClass({
         </div>
     },
     renderEatingStatusButtons: function () {
+        var buttons = ['fa-cutlery', 'fa-check-circle-o'];
         return <div className="preview-status-buttons">
-            <span className="preview-status-button-eat fa fa-check-circle-o"></span>
-            <span className="preview-status-button-eat fa fa-cutlery"></span>
+            {buttons.map(function (btnClass) {
+                return <span className={"preview-status-button-eat fa " + btnClass}></span>
+            })}
         </div>
     },
     renderFitnessStatusButtons: function () {
+        var buttons = ['fa-check-circle-o', 'fa-bicycle'];
         return <div className="preview-status-buttons">
-            <span className="preview-status-button-fit fa fa-check-circle-o"></span>
-            <span className="preview-status-button-fit fa fa-bicycle"></span>
+            {buttons.map(function (btnClass) {
+                return <span className={"preview-status-button-fit fa " + btnClass}></span>
+            })}
         </div>
     }
 });
